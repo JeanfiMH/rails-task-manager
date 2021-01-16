cfcb17099f6ec0711fb5099c1656dc6ebdc539fa
jeanfi mollheger <mollhegerjp@hotmail.com>
2021-01-16 12:03:00 +0100

    echec migration

---
+++ db/migrate/20210116104728_add_default_value_boolean_to_tast.rb
@@ -0,0 +1,5 @@
+class AddDefaultValueBooleanToTast < ActiveRecord::Migration[6.0]
+  def change
+    
+  end
+end
---
+++ db/migrate/20210116105137_add_default_value_for_completed_to_task.rb
@@ -0,0 +1,5 @@
+class AddDefaultValueForCompletedToTask < ActiveRecord::Migration[6.0]
+  def change
+    change_column :tasks, :completed, :boolean, default: false 
+  end
+end
